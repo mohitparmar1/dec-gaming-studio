d3e728210919ecb60143e01622022d083b209d06
Mohit Parmar <100152343+mohitparmar1@users.noreply.github.com>
2024-06-16 23:03:04 +0530

    minor ui changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage : {
+        "img" : "url('./src/assets/gaming-2.jpg')",
+      }
+    },
   },
   plugins: [],
 };
